b2d9bca53957b4bf7c95d6a8e104e99e7b952939
lapok <bhollenb96@gmail.com>
2026-03-11 16:26:02 +0800

    config: add docker configuration & initial dependencies

---
+++ src/test/java/org/example/reminderapp/ReminderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reminderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
